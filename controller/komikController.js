be9eb7b7910dabd7658a1fbe2881020a9c3e6f89
MOJI <apple@apples-MacBook-Air.local>
2026-07-22 11:02:23 +0700

    add res status komik

--- controller/komikController.js
+++ controller/komikController.js
@@ -17,4 +17,8 @@ async function getKomikById(req, res) {
         if (!komik) {
             return res.status(404).json({ error: 'Komik not found' });
         }
+        res.status(200).json(komik);
+    } catch (err) {
+        console.error('Error fetching komik by ID:', err.message);
+        res.status(500).json({ error: 'Failed to fetch komik by ID' });
     }
